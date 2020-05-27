18161001cc72a6ad16e7b0981bdabc26ca28c99a
danielcaldas <dcaldas@zendesk.com>
2020-05-27 22:29:58 +0800

    GIF component improvements. Autoplay, center provide alt as prop

--- content/posts/components/GIF.tsx
+++ content/posts/components/GIF.tsx
@@ -1,24 +1,36 @@
 import React, { useMemo, useState, FC } from "react";
 import exampleFinalResult from "../call-react-hooks-inside-conditions/example-final-result.gif";
+import travolta from "../about-css-conf-eu-berlin-2018/2.gif";
 import gifStyles from "./GIF.module.css";
 
+// https://github.com/benwiley4000/react-gif-player#options
 const Player = React.lazy(() => import("react-gif-player"));
 
 type Props = {
-  name: string;
-  width: string;
+  alt: string;
   height: string;
+  name: string;
+  playing: boolean;
+  width: string;
 };
 
-function GIF({ name, width = "100%", height = "100%" }: Props): FC {
+function GIF({
+  alt = "",
+  height = "100%",
+  name,
+  playing = false,
+  width = "100%",
+}: Props): FC {
   const isSSR = typeof window === "undefined";
-  const [playing, setPlaying] = useState(false);
+  const [isPlaying, setPlaying] = useState(playing);
   const src = useMemo(() => {
     switch (name) {
       case "example-final-result":
         return exampleFinalResult;
+      case "travolta":
+        return travolta;
       default:
-        return exampleFinalResult;
+        return "";
     }
   }, [name]);
 
@@ -26,16 +38,21 @@ function GIF({ name, width = "100%", height = "100%" }: Props): FC {
     <>
       {!isSSR && (
         <React.Suspense fallback={<div />}>
-          <div className={gifStyles.gifPlayer}>
-            {!playing && <div className={gifStyles.gifPlayButton}></div>}
-            {
-              <Player
-                width={width}
-                height={height}
-                gif={src}
-                onTogglePlay={() => setPlaying(!playing)}
-              />
-            }
+          <div className={gifStyles.gifPlayerContainer}>
+            <div className={gifStyles.gifPlayer}>
+              {!isPlaying && <div className={gifStyles.gifPlayButton}></div>}
+              {
+                <Player
+                  width={width}
+                  height={height}
+                  alt={alt}
+                  gif={src}
+                  playing={isPlaying}
+                  autoplay={isPlaying}
+                  onTogglePlay={() => setPlaying(!isPlaying)}
+                />
+              }
+            </div>
           </div>
         </React.Suspense>
       )}
