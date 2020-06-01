4e66962d27a51b1e126a08cc85ca8612a6d7fc3c
danielcaldas <dcaldas@zendesk.com>
2020-06-01 21:31:52 +0800

    Fix blc script. Update link to swapi domain was changed to .dev

--- blc.sh
+++ blc.sh
@@ -3,3 +3,8 @@
 blc -r http://localhost:9000/\
     --exclude https://www.linkedin.com/in/daniel-caldas/\
     --exclude http://localhost:9000/aboutme\
+    --exclude http://localhost:9000/rss.xml\
+    --exclude https://marketplace.visualstudio.com/items?itemName=jingkaizhao.vscode-redux-devtools\
+    --exclude https://github.com/philipwalton\
+    --exclude https://marketplace.visualstudio.com/items?itemName=msjsdiag.debugger-for-chrome\
+
