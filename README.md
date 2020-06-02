c5e8dc9d87ff982f1de70738928dd6f5af0947b7
Daniel Caldas <11733994+danielcaldas@users.noreply.github.com>
2020-06-02 21:41:52 +0800

    Chore/misc improvements cleanup (#4)
    * Slight change on bio
    
    * Fix vertical distances of source captions for images and gifs
    
    * Allow gatsby develop to be accessed within LAN
    
    * Remove unused imported components from blogposts
    
    * Gatsby serve on LAN
    
    * Update /aboutme page
    
    * Fix tagging
    
    * Improve job search article
    
    * Improved functional bits blog post
    
    * Improve please help deleted my git stash
    
    * Improve destructuring blog post
    
    * Improve unit testing with fixtures article
    
    * Improve unit tests fixture blogpost and remove the satisfying meme
    
    * Exclude amazon links from blc
    
    * Update README
    
    * Update sitemaps
    
    * Update gatsby-config metadata
    
    * Remove unecessary entry from package.json

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # goodguydaniel.com
-Blog and Home of Daniel Caldas
+
+<a href="https://goodguydaniel.com" title="Blog and Home of Daniel Caldas" target="_blank">Blog and Home of Daniel Caldas</a>
+
+## Run book
+
+### Development
+
+```
+npm run dev
+```
+
+### Production
+
+```
+npm run build && npm run serve
+```
--- gatsby-config.js
+++ gatsby-config.js
@@ -78,10 +78,9 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        name: "minimal-blog - @lekoarts/gatsby-theme-minimal-blog",
-        short_name: "minimal-blog",
-        description:
-          "Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.",
+        name: "goodguydaniel.com - Home and Blog of Daniel Caldas",
+        short_name: "goodguydaniel.com",
+        description: "Here is where Daniel writes about his experiences.",
         start_url: "/",
         background_color: "#fff",
         theme_color: "#6B46C1",
