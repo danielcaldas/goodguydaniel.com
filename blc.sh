7f0fa8dccf9f78808ec9f2738285bb344bd5e8fd
danielcaldas <caldasjdaniel@gmail.com>
2020-06-30 20:14:32 +0800

    Exclude tweak firefox link from blc check -.-

--- blc.sh
+++ blc.sh
@@ -8,4 +8,5 @@ blc -r http://localhost:9000/\
     --exclude https://marketplace.visualstudio.com/items?itemName=jingkaizhao.vscode-redux-devtools\
     --exclude https://github.com/philipwalton\
     --exclude https://marketplace.visualstudio.com/items?itemName=msjsdiag.debugger-for-chrome\
+    --exclude https://addons.mozilla.org/en-US/firefox/addon/tweak-extension/\
 
