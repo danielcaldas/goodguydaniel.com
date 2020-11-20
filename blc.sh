d4493524dc164bbfd55462f2803483a691a19ab9
danielcaldas <caldasjdaniel@gmail.com>
2020-11-20 18:25:38 +0800

    Fix blc script

--- blc.sh
+++ blc.sh
@@ -4,7 +4,6 @@ blc -r http://localhost:9000/\
     --exclude https://www.linkedin.com/in/daniel-caldas/\
     --exclude http://localhost:9000/aboutme\
     --exclude http://localhost:9000/rss.xml\
-    --exclude https://www.amazon.com/*\
     --exclude https://marketplace.visualstudio.com/items?itemName=jingkaizhao.vscode-redux-devtools\
     --exclude https://github.com/philipwalton\
     --exclude https://marketplace.visualstudio.com/items?itemName=msjsdiag.debugger-for-chrome\
