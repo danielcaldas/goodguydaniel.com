61565785a408fa82e5571e2e5d4c28b489ab3834
danielcaldas <caldasjdaniel@gmail.com>
2020-12-16 22:14:35 +0800

    Exclude github on blc

--- blc.sh
+++ blc.sh
@@ -7,4 +7,4 @@ blc -r http://localhost:9000/\
     --exclude https://marketplace.visualstudio.com/items?itemName=jingkaizhao.vscode-redux-devtools\
     --exclude https://marketplace.visualstudio.com/items?itemName=msjsdiag.debugger-for-chrome\
     --exclude https://addons.mozilla.org/en-US/firefox/addon/tweak-extension/\
-    --exlcude https://github.com/*\
+    --exclude https://github.com/*\
