cdf4047e7699da5b4ccde722df8a26072bc91586
danielcaldas <caldasjdaniel@gmail.com>
2020-12-16 22:24:34 +0800

    meh

--- blc.sh
+++ blc.sh
@@ -6,5 +6,4 @@ blc -r http://localhost:9000/\
     --exclude http://localhost:9000/rss.xml\
     --exclude https://marketplace.visualstudio.com/items?itemName=jingkaizhao.vscode-redux-devtools\
     --exclude https://marketplace.visualstudio.com/items?itemName=msjsdiag.debugger-for-chrome\
-    --exclude https://addons.mozilla.org/en-US/firefox/addon/tweak-extension/\
-    --exclude https://github.com/*\
+    --exclude https://addons.mozilla.org/en-US/firefox/addon/tweak-extension/
